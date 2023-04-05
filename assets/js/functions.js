d1d421b461fb1a3c8f165e88f3f069696df14f9c
Cerber2ol8 <Cerber2ol8@github.com>
2023-04-05 12:54:38 +0800

    修改css的主题配色，修改js navbar滚动行为，更换index页面的配色，修改index页面文本描述

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready(function() {
  
     $(window).scroll(function(){
-    $('.topnav').toggleClass('bg-white navbar-light shadow-sm scrollednav py-0', $(this).scrollTop() > 50);
+    $('.topnav').toggleClass('bg-black navbar-black shadow-sm scrollednav py-0', $(this).scrollTop() > 50);
     });
 
     $('#modal_newsletter').on('show.bs.modal', function () {
